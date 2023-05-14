56e08fc10656b9ff8faf8ab67b8c3bb2bf147ff4
kailyl <59887357+kailyl@users.noreply.github.com>
2023-05-13 22:07:48 -0400

    update redirect uri

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,6 +1,6 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 export const clientId = "3a73a562c583499ea19565fc3ebc4bcc";
-export const redirectUri = "http://localhost:3000";
+export const redirectUri = "https://plotify-app.github.io/";
 export const RESPONSE_TYPE = "token";
 export const scopes = [
     "user-top-read",
